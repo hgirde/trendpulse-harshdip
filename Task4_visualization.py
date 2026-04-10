8749aec127100d6c19df41fd6f6dbd4c0b1dbbcc
hgirde <harshdipgirde@gmail.com>
2026-04-10 16:27:44 +0530

    Update Task4_visualization.py

--- Task4_visualization.py
+++ Task4_visualization.py
@@ -1,14 +1,12 @@
-"""
+""
 Author:Harshdip Girde
-"""
+""
 
 
 
 import pandas as pd     
 import matplotlib.pyplot as plt 
 import os                
-
-
 
 
 file_path = "data/trends_analysed.csv"
@@ -20,17 +18,12 @@ except Exception as e:
     print("Error loading file:", e)
     exit()
 
-
-
 if not os.path.exists("outputs"):
     os.makedirs("outputs") 
 
 
-
 def shorten_title(title):
-    return title[:50] + "..." if len(title) > 50 else title  # bada title short
-
-
+    return title[:50] + "..." if len(title) > 50 else title  
 
 
 top10 = df.sort_values(by="score", ascending=False).head(10) 
@@ -47,8 +40,6 @@ plt.savefig("outputs/chart1_top_stories.png")
 plt.close()
 
 
-
-
 category_counts = df["category"].value_counts()
 
 plt.figure()
@@ -59,8 +50,6 @@ plt.title("Stories per Category")
 
 plt.savefig("outputs/chart2_categories.png")
 plt.close()
-
-
 
 
 popular = df[df["is_popular"] == True]
@@ -79,8 +68,6 @@ plt.savefig("outputs/chart3_scatter.png")
 plt.close()
 
 
-
-
 fig, axes = plt.subplots(1, 3, figsize=(18, 5)) 
 
 
@@ -90,11 +77,9 @@ axes[0].set_xlabel("Score")
 axes[0].invert_yaxis()
 
 
-
 axes[1].bar(category_counts.index, category_counts.values)
 axes[1].set_title("Stories per Category")
 axes[1].set_xlabel("Category")
-
 
 
 axes[2].scatter(popular["score"], popular["num_comments"], label="Popular")
@@ -102,7 +87,6 @@ axes[2].scatter(not_popular["score"], not_popular["num_comments"], label="Not Po
 axes[2].set_title("Score vs Comments")
 axes[2].set_xlabel("Score")
 axes[2].legend()
-
 
 plt.suptitle("TrendPulse Dashboard")
 
